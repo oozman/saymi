659df0de3241198f6ddf047e61577253decc6606
Omar <oomarusman@gmail.com>
2017-07-13 22:12:13 +0800

    Mi Humidifier command.
    - Update version.
    - Support Mi Humidifier.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const program = require('commander');
 const colors = require('colors');
 const AirPurifierCommand = require('./lib/commands/airpurifier_command');
+const HumidifierCommand = require('./lib/commands/humidifier_command');
 const DiscoverCommand = require('./lib/commands/discover_command');
 const helper = require('./lib/helper');
 
@@ -39,6 +40,22 @@ program
     .action(function (options) {
 
         return new AirPurifierCommand(program, options);
+
+    });
+
+/**
+ * Humidifier
+ */
+program
+    .command('humidifier')
+    .description(colors.yellow('This command allows you to control your Mi Humidifier.'))
+    .option('--get_data', 'Get device data, like: ' + 'device info'.bold + ', ' + 'sensor data'.bold + ', ' + 'etc'.bold + '.')
+    .option('--set_power <status>', 'Set device power status: ' + 'on'.bold + ' or ' + 'off'.bold)
+    .option('--get_modes', 'Get list of supported modes.')
+    .option('--set_mode <mode>', 'Set device mode.')
+    .action(function (options) {
+
+        return new HumidifierCommand(program, options);
 
     });
 
